33b15c00df5f4b6dcb0a3ad5f5bc59fce7a2c00c
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-06-16 15:51:49 +0530

    [rajaganapathi3076]creating a binary tree

---
+++ BinarySearch/MyBinaryNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public interface INode<T> where T : IComparable<T>
+    {
+        T Key { get; }
+        INode<T> Left { get; set; }
+        INode<T> Right { get; set; }
+    }
+    public class MyBinaryNode<T> : INode<T> where T : IComparable<T>
+    {
+        public T Key { get; }
+        public INode<T>? Left { get; set; }
+        public INode<T>? Right { get; set; }
+
+        public MyBinaryNode(T key)
+        {
+            Key = key;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private INode<T>? root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Add(T key)
+        {
+            root = AddNode(root, key);
+        }
+
+        private INode<T> AddNode(INode<T> node, T key)
+        {
+            if (node == null)
+            {
+                node = new MyBinaryNode<T>(key);
+            }
+            else if (key.CompareTo(node.Key) < 0)
+            {
+                node.Left = AddNode(node.Left, key);
+            }
+            else if (key.CompareTo(node.Key) > 0)
+            {
+                node.Right = AddNode(node.Right, key);
+            }
+
+            return node;
+        }
+    
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,21 @@
+﻿using BinarySearch;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+        bst.Add(56);
+        bst.Add(30);
+        bst.Add(70);
+        bst.Add(96);
+        bst.Add(37);
+        bst.Add(88);
+
+        // Perform any additional operations on the BST as needed
+
+        Console.WriteLine("Binary Search Tree created successfully!");
+    }
+}
